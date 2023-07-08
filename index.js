9483c760e00c15fb698bd238d7b90a6438caec26
Wissididom <webmaster@wissididom.me>
2023-07-08 22:10:07 +0200

    Revert index.js Merge mistake

--- index.js
+++ index.js
@@ -35,48 +35,44 @@ async function getGuid(jwtToken, broadcasterLogin) {
 (async function () {
   if (process.env["JWT_TOKEN"]) {
     let jwtToken = process.env["JWT_TOKEN"];
-    let broadcasterLogin = process.env["BROADCASTER_LOGIN"] || "me";
+    let guids = process.env["STREAMELEMENTS_ACCOUNT_IDS"] || "me";
     let textMessage = process.env["TEXT_MESSAGE"] || "TEXT_MESSAGE not set!";
-    for (let login of broadcasterLogin.split(",")) {
-      console.log(`Login:${login}`);
-      await fetch(`https://api.streamelements.com/kappa/v2/channels/${login}`, {
+    for (let guid of guids.split(",")) {
+      if (guid == "me") guid = await getGuid(jwtToken, guid);
+      if (!isStreamElementsAccountId(guid)) {
+        console.warn(`${guid} is not a valid StreamElements Account ID!`);
+        if (isTwitchLoginname(guid.toLowerCase())) {
+          console.log(
+            `Attempting to load the StreamElements Account ID from the Twitch login name!`,
+          );
+          guid = await getGuid(jwtToken, guid);
+        } else {
+          console.warn(
+            `${guid} is not a Twitch login name either! Ignoring Account!`,
+          );
+          continue;
+        }
+      }
+      await fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
+        method: "POST",
         headers: {
-          Accept: "application/json",
+          Accept: "application/json; charset=utf-8",
           Authorization: `Bearer ${jwtToken}`,
           "Content-Type": "application/json",
         },
+        body: JSON.stringify({
+          message: textMessage,
+        }),
       })
         .then((res) => {
-          console.log(`channels/${login}: Response-Code ${res.status}`);
+          console.log(`bot/${guid}/say: Response-Code ${res.status}`);
           return res.json();
         })
         .then((data) => {
-          console.log(`channels/${login}: ${JSON.stringify(data)}`);
-          let guid = data._id;
-          fetch(`https://api.streamelements.com/kappa/v2/bot/${guid}/say`, {
-            method: "POST",
-            headers: {
-              Accept: "application/json; charset=utf-8",
-              Authorization: `Bearer ${jwtToken}`,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              message: textMessage,
-            }),
-          })
-            .then((res) => {
-              console.log(`bot/${guid}/say: Response-Code ${res.status}`);
-              return res.json();
-            })
-            .then((data) => {
-              console.log(`bot/${guid}/say: ${JSON.stringify(data)}`);
-            });
-        })
-        .catch((err) => {
-          console.log(`Error getting User Guid: ${err}`);
+          console.log(`bot/${guid}/say: ${JSON.stringify(data)}`);
         });
     }
   } else {
-    console.log("Missing JWT_TOKENS in .env file");
+    console.log("Missing JWT_TOKEN in .env file");
   }
 })();
